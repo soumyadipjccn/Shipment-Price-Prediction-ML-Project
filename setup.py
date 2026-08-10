01347496971440729c645462f8ac990d2efc110c
soumyadipjccn <mailsoumyadipghosh@gmail.com>
2026-08-10 15:21:21 +0000

    All Package are successful installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Soumyadip",
+    author_email="soumyadip@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
